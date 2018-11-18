cb1d123892d003a014cdb0fc3fea50f6d49aea89
Md Sumsuddin <sumsuddinshojib@gmail.com>
2018-11-19 01:45:29 +0600

    cleaned up httpd for only streaming image

--- mjpg-streamer-experimental/plugins/output_http/httpd.h
+++ mjpg-streamer-experimental/plugins/output_http/httpd.h
@@ -83,16 +83,7 @@ static const struct {
 /* the webserver determines between these values for an answer */
 typedef enum {
     A_UNKNOWN,
-    A_SNAPSHOT,
-    A_SNAPSHOT_WXP,
     A_STREAM,
-    A_STREAM_WXP,
-    A_COMMAND,
-    A_FILE,
-    A_CGI,
-    A_INPUT_JSON,
-    A_OUTPUT_JSON,
-    A_PROGRAM_JSON,
     #ifdef MANAGMENT
     A_CLIENTS_JSON
     #endif
@@ -172,10 +163,6 @@ typedef struct {
 /* prototypes */
 void *server_thread(void *arg);
 void send_error(int fd, int which, char *message);
-void send_output_JSON(int fd, int plugin_number);
-void send_input_JSON(int fd, int plugin_number);
-void send_program_JSON(int fd);
-void check_JSON_string(char *source, char *destination);
 
 #ifdef MANAGMENT
 client_info *add_client(char *address);
